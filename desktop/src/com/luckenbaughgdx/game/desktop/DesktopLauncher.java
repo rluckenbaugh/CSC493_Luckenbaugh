7a409b9b26f23932a0525ee8f2dd26caff6f86d9
Renae Luckenbaugh <rl6565@ship.edu>
2016-08-31 09:06:45 -0400

    initial commit of all base files for libgdx project

---
+++ desktop/src/com/luckenbaughgdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.luckenbaughgdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.luckenbaughgdx.game.LuckenbaughGdxGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new LuckenbaughGdxGame(), config);
+	}
+}
